161ebfcb15e7a7ffb826cf6a9b721937f9952a6f
sebasdrk17 <alexis7_hernandez14@hotmail.com>
2021-02-11 19:14:06 -0600

    [ADD] Details product view

--- Store/App.js
+++ Store/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Providers from './src/navigation';
+import ProductDetails from './src/screens/ProductDetails'
 
 const App = () => {
-  return <Providers />;
+  //return <Providers />;
+  return <ProductDetails />
 };
 
 export default App;
