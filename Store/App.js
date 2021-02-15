6fde78abfb211aeb49e5cf6026151abdc804db1c
jjcr11 <jjcr0397@gmail.com>
2021-02-15 10:48:18 -0600

    Create new branch

--- Store/App.js
+++ Store/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Providers from './src/navigation';
-import ProductDetails from './src/screens/ProductDetails'
+import ProductDetails from './src/screens/ProductDetails';
 
 const App = () => {
-  //return <Providers />;
-  return <ProductDetails />
+  return <Providers />;
+  //return <ProductDetails />
 };
 
 export default App;
